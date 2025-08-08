323fe86c0d9b0349914c995d25f27d33f9f8d338
Ian Pösse-Koch <ian-coding@poesseko.ch>
2025-08-08 23:57:12 +0200

    fix(backend): add error handling to server runner

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,11 @@
 import app from './app';
 import config from './config/config';
+import loggingService from './services/logging/logging.service';
 
-app.listen(config.port, () => {
+app.listen(config.port, (error) => {
+  if (error) {
+    loggingService.log(error, "ERROR");
+  }
   console.log(`Server running on port ${config.port}`);
 });
+
