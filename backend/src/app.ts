2d33eeca18ba57456019a6c54957d6eadad58fc5
Ian Pösse-Koch <ian-coding@poesseko.ch>
2025-08-09 15:15:31 +0200

    feat(backend): enable cors requests

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 import platformRoutes from './routes/platform.routes';
 import errorHandler from './services/handlers/error.handler';
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.json());
+
+app.use(cors());
 
 // Routes
 app.use('/platforms', platformRoutes.router);
