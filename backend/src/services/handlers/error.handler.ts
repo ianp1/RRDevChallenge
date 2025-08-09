bed9d79b21dfd259e2f6a4d2bdad1126fa43b090
Ian Pösse-Koch <ian-coding@poesseko.ch>
2025-08-10 01:31:57 +0200

    feat: improve error messages

--- backend/src/services/handlers/error.handler.ts
+++ backend/src/services/handlers/error.handler.ts
@@ -5,9 +5,15 @@ class ExpressErrorHandler {
   handleError(err: Error, req: Request, res: Response, next: NextFunction) {
     loggingService.log(`Error when processing request ${req.url}`, 'ERROR');
     loggingService.log(err, 'ERROR');
+    if (err.message === "Gateway Timeout") {
+      res.status(500).send({
+        error: "Gateway Timeout",
+        message: "Keine Antwort vom DB Server. Das kann an einer zu hohen Anzahl von Anfragen des Nutzers liegen. Bitte versuchen Sie es nach einer kurzen Pause erneut"
+      });
+      return;
+    }
 
     res.status(500).send();
-    next();
   }
 }
 
--- frontend/src/app/services/error-display-service.ts
+++ frontend/src/app/services/error-display-service.ts
@@ -14,24 +14,42 @@ export class ErrorDisplayService {
     //In production, this could be replaced with an optional telemetrics service, collecting error information from clients 
     console.log("An Error occured:");
     console.log(error);
+    let rawError:any = error;
     
     var errorMessage = "";
     var errorTitle = "";
-    if (error instanceof ValidationError) {
-      errorTitle = "Kommunikationsfehler";
-      errorMessage = "Beim Abrufen der Daten vom Server ist ein Fehler aufgetreten. Die erhaltenen Daten stimmen nicht mit dem erwarteten Format überein. Eventuell sind die Versionen inkompatibel";
-    } else if (error instanceof Error){
-      errorTitle = "Unerwarteter Fehler";
-      errorMessage = `${JSON.stringify(error.message)}
-      
-      Stack: ${error.stack}`;
-    } else {
-      errorTitle = "Unerwarteter Fehler";
-      errorMessage = JSON.stringify(error);
+
+    //Unfortunately, the http errors are not strictly typed. 
+    // Thats why we need to construct the object in this way to recover eventual error messages
+    if (Object.hasOwn(rawError, "error")) {
+      if (Object.hasOwn(rawError["error"], "error")) {
+        if (rawError["error"]["error"] === "Gateway Timeout") {
+          errorMessage = rawError["error"]["message"];
+          errorTitle = "DB Serverfehler";
+        }
+      }
+    }
+    console.log(rawError['error']['error']);
+    //console.log(rawError.error)
+    if (errorTitle=== "") {
+      if (error instanceof ValidationError) {
+        errorTitle = "Kommunikationsfehler";
+        errorMessage = "Beim Abrufen der Daten vom Server ist ein Fehler aufgetreten. Die erhaltenen Daten stimmen nicht mit dem erwarteten Format überein. Eventuell sind die Versionen inkompatibel";
+      } else if (error instanceof Error){
+        errorTitle = "Unerwarteter Fehler";
+        errorMessage = `${JSON.stringify(error.message)}
+        
+        Stack: ${error.stack}`;
+      } else {
+        errorTitle = "Unerwarteter Fehler";
+        errorMessage = JSON.stringify(error);
+      }
+
     }
 
+
     console.log(errorMessage);
-    var dialogRef = this.dialog.open(ErrorDisplayDialog, {
+    this.dialog.open(ErrorDisplayDialog, {
       data: {
         message: errorMessage,
         title: errorTitle
