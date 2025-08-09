99a07ee375c475aa7e14d55b83d65c178bc970b7
Ian Pösse-Koch <ian-coding@poesseko.ch>
2025-08-09 14:55:46 +0200

    feat(frontend): add basic page layout

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,9 +1,28 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PlatformSearch } from './features/platform-search/platform-search';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet, 
+    PlatformSearch,
+    MatIconModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatListModule,
+    MatSlideToggleModule
+
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
